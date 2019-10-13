a051dfb77e139fcca95087e868376baacadfdcfe
Brandt Strom <brandtkstrom@gmail.com>
2019-10-12 18:02:19 -0700

    initial express app and http server

---
+++ server.ts
@@ -0,0 +1,14 @@
+import express, { Application } from 'express';
+import morgan from 'morgan'
+import DB from './db';
+
+const app: Application = express();
+app.use(morgan('dev'));
+
+const PORT: string = process.env.PORT || '6969';
+
+// Listen for HTTP traffic once DB connection is established
+DB.once('open', () => {
+    console.log('Successfully connected to DB.');
+    app.listen(PORT, () => console.log(`Listening for connections on port: ${PORT}`));
+});
