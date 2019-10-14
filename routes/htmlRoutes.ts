c133c1bc7e792d4cf32dee83820ba3d50699b9a4
Brandt Strom <brandtkstrom@gmail.com>
2019-10-13 17:38:55 -0700

    use sendFile & specify root dir for index.html

--- routes/htmlRoutes.ts
+++ routes/htmlRoutes.ts
@@ -1,7 +1,10 @@
+import path from 'path';
 import { Router } from 'express';
 
 export const HtmlRoutes = Router().get('*', (req, res) => {
   // Defer '/' and all other routes to React App
   res.setHeader('Content-Type', 'text/html');
-  res.status(200).send('index.html');
+  res.status(200).sendFile('index.html', {
+    root: path.join(__dirname, '../client/build')
+  });
 });
