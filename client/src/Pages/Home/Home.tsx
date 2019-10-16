887e6e95112f55cf672df9058c47bf788c2b714f
Nich Secord <nicholassecord@gmail.com>
2019-10-15 18:47:58 -0700

    started search page

--- client/src/Pages/Home/Home.tsx
+++ client/src/Pages/Home/Home.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import { Card } from 'Elements';
 
 interface Props {}
 
 const Home: React.FC<Props> = () => {
-  return <>home page</>;
+  return <WelcomeCard>welcome to the manhattan project</WelcomeCard>;
 };
 
 export default Home;
+
+const WelcomeCard = styled(Card).attrs({ as: 'section' })``;
