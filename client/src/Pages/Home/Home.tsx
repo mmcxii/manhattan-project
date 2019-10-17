44fdba5dbce89283644bdbd67a1b23ce45a57cea
Nich Secord <nicholassecord@gmail.com>
2019-10-17 11:56:47 -0700

    new card markup

--- client/src/Pages/Home/Home.tsx
+++ client/src/Pages/Home/Home.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { Card } from 'Elements';
+import { Card, CardHeader, CardBody } from 'Elements';
 
 interface Props {}
 
 const Home: React.FC<Props> = () => {
-  return <WelcomeCard>welcome to the manhattan project</WelcomeCard>;
+  return (
+    <WelcomeCard>
+      <CardHeader>the manhattan project</CardHeader>
+      <CardBody>welcome to the manhattan project</CardBody>
+    </WelcomeCard>
+  );
 };
 
 export default Home;
 
-const WelcomeCard = styled(Card).attrs({ as: 'section' })``;
+const WelcomeCard = styled(Card).attrs({ as: 'section' })`
+  margin: auto;
+`;
