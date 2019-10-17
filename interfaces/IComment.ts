390be98201ba75c1e0807e1de03b7e75d8adb329
Brandt Strom <brandtkstrom@gmail.com>
2019-10-16 22:19:24 -0700

    Comment voting methods

--- interfaces/IComment.ts
+++ interfaces/IComment.ts
@@ -3,4 +3,6 @@ import { IUser } from '../interfaces/IUser';
 export interface IComment {
     author: IUser;
     text: string;
+    upvotes: IUser[];
+    downvotes: IUser[];
 }
--- models/Comment.ts
+++ models/Comment.ts
@@ -6,8 +6,8 @@ import { ObjectID } from 'bson';
 // Create interface for Comment documents
 export interface ICommentDocument extends IComment, Document {
   rating: number;
-  upvote(user: IUserDocument): Promise<number>;
-  downvote(user: IUserDocument): Promise<number>;
+  upvote(user: IUserDocument): Promise<number | Error>;
+  downvote(user: IUserDocument): Promise<number | Error>;
 }
 
 // Create interface for Comment model
@@ -25,14 +25,18 @@ const commentSchema = new Schema({
     type: Types.String,
     required: true
   },
-  upvotes: {
-    type: [Types.ObjectId],
-    ref: 'User'
-  },
-  downvotes: {
-    type: [Types.ObjectId],
-    ref: 'User'
-  }
+  upvotes: [
+    {
+      type: Types.ObjectId,
+      ref: 'User'
+    }
+  ],
+  downvotes: [
+    {
+      type: Types.ObjectId,
+      ref: 'User'
+    }
+  ]
 });
 
 commentSchema
@@ -45,7 +49,7 @@ commentSchema
   });
 
 // Comment schema method to create a new comment
-commentSchema.static('createComment', async function(
+commentSchema.statics.createComment = async function(
   username: string,
   comment: string
 ): Promise<ICommentDocument | Error> {
@@ -58,49 +62,54 @@ commentSchema.static('createComment', async function(
   const text = comment.trim().toLowerCase();
 
   return Comment.create({ author: user._id, text });
-});
+};
 
 // User upvoting comment
 commentSchema.methods.upvote = async function(
   this: { _id: ObjectID },
   user: IUserDocument
-): Promise<number> {
+): Promise<number | Error> {
   // Create query update uptions to add user id to upvotes and remove from downvotes
   const options: QueryUpdateOptions = {
     $pull: { downvotes: user._id },
     $addToSet: { upvotes: user._id }
   };
 
-  return addVote(this._id, options);
+  return updateVotes(this._id, options, 'upvote');
 };
 
 // User downvoting comment
 commentSchema.methods.downvote = async function(
   this: { _id: ObjectID },
   user: IUserDocument
-): Promise<number> {
+): Promise<number | Error> {
   // Create query update uptions to add user id to downvotes and remove from upvotes
   const options: QueryUpdateOptions = {
     $pull: { upvotes: user._id },
     $addToSet: { downvotes: user._id }
   };
 
-  return addVote(this._id, options);
+  return updateVotes(this._id, options, 'downvote');
 };
 
 // Updates a comment's rating
-const addVote = async function(id: ObjectID, options: QueryUpdateOptions): Promise<number> {
+const updateVotes = async function(
+  id: ObjectID,
+  options: QueryUpdateOptions,
+  type: 'upvote' | 'downvote'
+): Promise<number | Error> {
+  let comment: ICommentDocument | null;
   try {
-    const result = await Comment.updateOne({ _id: id }, options);
+    comment = await Comment.findOneAndUpdate({ _id: id }, options, { new: true });
 
-    if (!result || !result.ok) {
-      return 500;
+    if (!comment) {
+      return new Error(`Comment ${id} not found.`);
     }
   } catch (error) {
-    return 500;
+    return new Error(`Could not update comment votes: ${error}`);
   }
 
-  return 200;
+  return type === 'upvote' ? comment.upvotes.length : comment.downvotes.length;
 };
 
 export const Comment = model<ICommentDocument, ICommentModel>('Comment', commentSchema);
