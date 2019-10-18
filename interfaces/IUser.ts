c35b6510af3a4f1c2d992a52e5dc88ebffb51903
Brandt Strom <brandtkstrom@gmail.com>
2019-10-17 20:07:33 -0700

    make IUser follow/followers not optional/nullable

--- interfaces/IUser.ts
+++ interfaces/IUser.ts
@@ -1,8 +1,8 @@
 export interface IUser {
   username: string;
+  follows: IUser[];
+  followers: IUser[];
   name?: string;
   age?: number;
   bio?: string;
-  follows?: IUser[];
-  followers?: IUser[];
 }
