9cfb9cc84aee4711cd685b83b18d0f674737aa2a
Nich Secord <nicholassecord@gmail.com>
2019-10-22 20:12:55 -0700

    added form for editing user information

--- client/src/Pages/EditUser/EditUser.tsx
+++ client/src/Pages/EditUser/EditUser.tsx
@@ -1,19 +1,86 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom';
 
-import { UserProps } from 'Store';
+import { UserProps, UserContext } from 'Store';
+import { useForm } from 'Hooks';
+import { Card, Form, Input, Button, CardBody, CardHeader } from 'Elements';
 
 interface Props {}
 
 const EditUser: React.FC<Props> = () => {
-  const [userInfo, setUserInfo] = useState<UserProps | null>(null);
+  const { username } = useParams();
+  const { dispatch } = useContext(UserContext);
+  //@ts-ignore
+  const initialState: UserProps = JSON.parse(localStorage.getItem('userInfo'));
+  //@ts-ignore
+  const loginToken: string = localStorage.getItem('loginToken');
+  const [values, handleChange] = useForm({
+    theme: '',
+    name: '',
+    age: '',
+    bio: '',
+    highlightedFavorite: '',
+    ...initialState,
+  });
 
-  useEffect(() => {
-    const getUserInfo = async () => {};
+  const saveDataToDataBase = async () => {
+    try {
+      const response: Response = await fetch(`/api/users/${username}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${loginToken}`,
+        },
+        body: JSON.stringify(values),
+      });
+      const data: UserProps = await response.json();
 
-    getUserInfo();
-  }, []);
+      const userData: UserProps = {
+        username: data.username,
+        theme: data.theme,
+        follows: data.follows,
+        followers: data.followers,
+      };
+      if (data.name) {
+        userData.name = data.name;
+      }
+      if (data.age) {
+        userData.age = data.age;
+      }
+      if (data.bio) {
+        userData.bio = data.bio;
+      }
 
-  return <>Edit User Info Page</>;
+      localStorage.setItem('userInfo', JSON.stringify(userData));
+      dispatch({ type: 'LOG_USER_IN', payload: userData });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <>
+      <Card as='section'>
+        <CardHeader>Edit Profile</CardHeader>
+        <CardBody>
+          <Form
+            onSubmit={e => {
+              e.preventDefault();
+
+              saveDataToDataBase();
+            }}
+          >
+            <Input name='name' value={values.name} onChange={handleChange} />
+            <Input name='age' value={values.age} onChange={handleChange} type='number' />
+            <Input name='bio' value={values.bio} onChange={handleChange} type='textarea' />
+            {/* TODO: Make <Toggle /> component for toggling theme */}
+
+            <Button type='submit'>Save Changes</Button>
+          </Form>
+        </CardBody>
+      </Card>
+    </>
+  );
 };
 
 export default EditUser;
