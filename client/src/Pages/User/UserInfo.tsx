cdd82d75de64aac0ba11df5d32ef1051b8af5c5b
Nich Secord <nicholassecord@gmail.com>
2019-10-22 17:42:01 -0700

    added route for edit user page

--- client/src/Pages/User/UserInfo.tsx
+++ client/src/Pages/User/UserInfo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { UserProps } from 'Store';
-import { Button, Card, CardHeader, CardBody } from 'Elements';
+import { ButtonLink, Card, CardHeader, CardBody } from 'Elements';
 import { spacing } from 'Utilities';
 
 interface Props {
