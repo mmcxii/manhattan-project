2d456812467fc4f138ce2be98b1bc040d9033689
Remeto <remetojohn@gmail.com>
2019-10-25 13:32:53 -0700

    some tests for user model

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: '@shelf/jest-mongodb',
+  transform: {
+    '^.+\\.ts?$': 'ts-jest'
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  collectCoverage: true,
+  clearMocks: true,
+  coverageDirectory: 'coverage'
+};
