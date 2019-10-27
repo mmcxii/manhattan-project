1fde297d205dc99d517dbb535c9636e85f95f8b5
Brandt Strom <brandtkstrom@gmail.com>
2019-10-27 13:43:27 -0700

    create transform utilities

--- client/src/Utilities/index.ts
+++ client/src/Utilities/index.ts
@@ -7,5 +7,6 @@ import transition from './transition';
 export * from './colors';
 export * from './fade';
 export * from './rounded';
+export * from './transform';
 
 export { absolute, colors, elevation, spacing, transition };
