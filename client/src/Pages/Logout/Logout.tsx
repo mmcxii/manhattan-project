f062054fb7888c165dc5929edef77331d3eeffa8
Nich Secord <nicholassecord@gmail.com>
2019-10-28 13:36:29 -0700

    completed logout page

--- client/src/Pages/Logout/Logout.tsx
+++ client/src/Pages/Logout/Logout.tsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import styled from 'styled-components';
+import { useHistory } from 'react-router';
+
+import { Card, CardHeader, CardBody, Button } from 'Elements';
+import { UserContext } from 'Store';
 
 interface Props {}
 
 const Logout: React.FC<Props> = () => {
-  return <>logout page</>;
+  const { dispatch } = useContext(UserContext);
+  const { push } = useHistory();
+
+  const logUserOut = () => {
+    // Remove user info from localstorage and UserContext
+    localStorage.removeItem('loginToken');
+    localStorage.removeItem('userInfo');
+    dispatch({ type: 'LOG_USER_OUT' });
+
+    // Redirect the user to the home page
+    push('/');
+  };
+
+  return (
+    <LogoutCard>
+      <CardHeader>Log out</CardHeader>
+      <CardBody>
+        <p>
+          To log out of <cite>The Manhattan Project</cite> click the button below.
+        </p>
+        <Button onClick={logUserOut}>Click here to log out</Button>
+      </CardBody>
+    </LogoutCard>
+  );
 };
 
 export default Logout;
+
+const LogoutCard = styled(Card).attrs({ as: 'section' })`
+  margin: auto;
+`;
