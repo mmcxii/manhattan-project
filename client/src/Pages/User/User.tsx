4379bd01981dab481cb75a0d987606a432cf285a
Brandt Strom <brandtkstrom@gmail.com>
2019-11-06 18:38:47 -0800

    render user info after load

--- client/src/Pages/User/User.tsx
+++ client/src/Pages/User/User.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 
 import { UserProps } from 'Store';
 import { useTitle } from 'Hooks';
@@ -9,14 +10,25 @@ import FollowsAndFollowers from './FollowsAndFollowers';
 
 interface Props {}
 
+const ErrorText = styled.p`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  justify-content: center;
+  margin: 2em 0;
+  font-size: 1.5rem;
+`;
+
 const User: React.FC<Props> = () => {
   const { username } = useParams();
   const [profileInfo, setProfileInfo] = useState<UserProps | null>(null);
+  const [displayUser, setDisplayUser] = useState<boolean>(false);
   useTitle(profileInfo ? profileInfo.name || profileInfo.username : 'Error: User not found');
 
   useEffect(() => {
     const getUser = async () => {
       try {
+        setDisplayUser(false);
         // Get profile information from database
         const response: Response = await fetch(`/api/users/${username}`, { method: 'GET' });
         const data: UserProps = await response.json();
@@ -24,27 +36,27 @@ const User: React.FC<Props> = () => {
         setProfileInfo(data);
       } catch (err) {
         console.log(err);
+      } finally {
+        setDisplayUser(true);
       }
     };
 
     getUser();
   }, [username]);
 
-  return (
+  const profile = profileInfo ? (
     <>
-      {profileInfo ? (
-        <>
-          <UserInfo profileInfo={profileInfo} />
+      <UserInfo profileInfo={profileInfo} />
 
-          <FavoritesSection profileInfo={profileInfo} />
+      <FavoritesSection profileInfo={profileInfo} />
 
-          <FollowsAndFollowers profileInfo={profileInfo} />
-        </>
-      ) : (
-        <p>Error: No User was found with that name.</p>
-      )}
+      <FollowsAndFollowers profileInfo={profileInfo} />
     </>
+  ) : (
+    <ErrorText>No user was found with that name.</ErrorText>
   );
+
+  return <>{displayUser && profile}</>;
 };
 
 export default User;
